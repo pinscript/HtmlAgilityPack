bb7b3334c1a8cb12212a17da327e41a4c99fd24f
alexandernyquist <nyquist.alexander@gmail.com>
2013-04-03 19:03:53 +0200

    Initial commit

---
+++ Branches/2.0.Experimental/HtmlAgilityPack.Tests/XpathTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace HtmlAgilityPack.Tests
+{
+    [TestFixture]
+    class XpathTests
+    {
+        [Test]
+        public void TestHrefAttributeSelection()
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(
+                "<html><body><div><p><a href=\"http://somewhereoutthere.com\">adfasf </a></p></div><p><a href=\"asdfadsf asdfasdf\">asdfa</a></p><body></html>");
+            var nodes = doc.DocumentNode.SelectNodes("//@href");
+            Assert.AreEqual(2, nodes.Count);
+            Assert.AreEqual(nodes.OfType<HtmlAttribute>().Count(), nodes.Count);
+        }
+
+        [Test]
+        public void TestHrefNodeSelection()
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(
+                "<html><body><div><p><a href=\"http://somewhereoutthere.com\">adfasf </a></p></div><p><a href=\"asdfadsf asdfasdf\">asdfa</a></p></body></html>");
+            var nodes = doc.DocumentNode.SelectNodes("//*[@href]");
+            Assert.AreEqual(2, nodes.Count);
+            Assert.AreEqual(nodes.OfType<HtmlNode>().Count(), nodes.Count);
+        }
+        [Test]
+        public void SelectAllChildNodesOfElement()
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(
+                "<html><body><div><p><a href=\"http://somewhereoutthere.com\">adfasf </a></p></div><p><a href=\"asdfadsf asdfasdf\">asdfa</a></p></body></html>");
+            var nodes = doc.DocumentNode.SelectNodes("html/body/*");
+            Assert.AreEqual(2, nodes.Count);
+            Assert.AreEqual(nodes.OfType<HtmlNode>().Count(), nodes.Count);
+        }
+
+        [Test]
+        public void TestHrefAttributeValueSelection()
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(
+                "<html><body><div><p><a href=\"http://somewhereoutthere.com\">adfasf </a></p></div><p><a href=\"asdfadsf asdfasdf\">asdfa</a></p><body></html>");
+            var nodes = doc.DocumentNode.SelectNodes("//*[@href='http://somewhereoutthere.com']");
+            Assert.AreEqual(1, nodes.Count);
+            Assert.AreEqual(nodes.OfType<HtmlNode>().Count(), nodes.Count);
+        }
+    }
+}
---
+++ Branches/2.0.Experimental/HtmlAgilityPack/IHtmlBaseNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HtmlAgilityPack
+{
+    public interface IHtmlBaseNode
+    {
+        string Name { get; set; }
+        bool HasAttributes { get;  }
+        HtmlAttributeCollection Attributes { get; }
+        HtmlNodeCollection ChildNodes { get; }
+        HtmlNodeType NodeType { get; }
+        string InnerText { get; }
+        HtmlNode ParentNode { get; }
+        bool HasChildNodes { get; }
+        HtmlNode NextSibling { get; }
+        HtmlNode CloneNode(bool something);
+        HtmlNode PreviousSibling { get; }
+    }
+}
